9f6b9b57ba178d2d276c25eb55f137aad319e6de
Roland <roland.pistache@gmail.com>
2023-04-09 21:30:51 +0200

    0.1

--- horizon/theme_description.php
+++ horizon/theme_description.php
@@ -1,7 +1,7 @@
 <?php
 $theme_description['name'] = 'Horizon';
 $theme_description['author'] = 'Roland Ti - <a href="https://github.com/RolandTi/horizon ">GitHub</a>';
-$theme_description['version'] = '0.0';
-$theme_description['date'] = '08/04/23';
+$theme_description['version'] = '0.1';
+$theme_description['date'] = '09/04/23';
 $theme_description['desc'] = 'Horizon is a responsive theme for ZenphotoCMS.';
 ?>
